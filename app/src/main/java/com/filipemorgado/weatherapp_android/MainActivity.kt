8757b6317bff95a66e84ba9285be99f20814d652
Filipe Morgado <filipedrmorgado@hotmail.com>
2023-02-14 01:08:32 +0000

    Initial commit

---
+++ app/src/main/java/com/filipemorgado/weatherapp_android/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.filipemorgado.weatherapp_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.filipemorgado.weatherapp_android.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/filipemorgado/weatherapp_android/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.filipemorgado.weatherapp_android.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
