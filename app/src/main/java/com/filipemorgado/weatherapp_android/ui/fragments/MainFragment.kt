25604d9d612686656a0e57150503ad431e99ae4e
Filipe Morgado <filipedrmorgado@hotmail.com>
2023-02-25 23:46:16 +0000

    - Initialize Recycler

--- app/src/main/java/com/filipemorgado/weatherapp_android/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/filipemorgado/weatherapp_android/ui/fragments/MainFragment.kt
@@ -6,27 +6,50 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.filipemorgado.weatherapp_android.databinding.FragmentMainBinding
+import com.filipemorgado.weatherapp_android.ui.adapters.MultipleDaysRecyclerView
 import com.filipemorgado.weatherapp_android.ui.viewmodels.WeatherViewModel
 
 class MainFragment : Fragment() {
 
     private lateinit var binding: FragmentMainBinding
-    private val viewModel: WeatherViewModel by activityViewModels()
+    private val weatherViewModel: WeatherViewModel by activityViewModels()
 
+    private lateinit var multipleDaysRecyclerView: MultipleDaysRecyclerView
     //private lateinit var
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentMainBinding.inflate(inflater, container, false)
+
+        // First time setup
+        setupUI()
         return binding.root
     }
 
+    /**
+     * Setup initial UI
+     */
+    private fun setupUI() {
+        initializeRecyclerView()
+    }
+
+    private fun initializeRecyclerView() {
+        multipleDaysRecyclerView = MultipleDaysRecyclerView()
+        val mLayoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.HORIZONTAL,
+            false
+        )
+        binding.contentMainLayout.recyclerView.apply {
+            layoutManager = mLayoutManager
+            itemAnimator = DefaultItemAnimator()
+            adapter = multipleDaysRecyclerView
+        }
+    }
 
 }
