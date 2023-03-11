239fc8f6fb22a91a6272c85ec9d79628e72fbb92
Filipe Morgado <filipedrmorgado@hotmail.com>
2023-03-11 23:28:55 +0000

    - Added transparent shadow effect to recycler forecast

--- app/src/main/java/com/filipemorgado/weatherapp_android/ui/adapters/MultipleDaysRecyclerView.kt
+++ app/src/main/java/com/filipemorgado/weatherapp_android/ui/adapters/MultipleDaysRecyclerView.kt
@@ -6,10 +6,11 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.alpha
+import androidx.core.graphics.ColorUtils
 import androidx.recyclerview.widget.RecyclerView
 import com.filipemorgado.weatherapp_android.data.model.response.ForecastDay
 import com.filipemorgado.weatherapp_android.databinding.WeatherDayItemBinding
+import com.filipemorgado.weatherapp_android.utils.ALPHA_RECYCLER_SHADOW_COLOR
 import com.filipemorgado.weatherapp_android.utils.AppUtils
 import com.filipemorgado.weatherapp_android.utils.RECYCLER_SIZE
 import java.util.*
@@ -48,34 +49,34 @@ class MultipleDaysRecyclerView : RecyclerView.Adapter<MultipleDaysRecyclerView.V
                 this.minTempTextView.text = forecastDayDetails.day.mintempC.toString()
                 this.maxTempTextView.text = forecastDayDetails.day.maxtempC.toString()
                 this.tempTextView.text = forecastDayDetails.day.avgtempC.toString()
+
                 // Setting weekday //todo bug here, check weekday text.
                 calendar.timeInMillis = forecastDayDetails.dateEpoch
                 val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
                 this.dayNameTextView.text = AppUtils.DAYS_OF_WEEK[dayOfWeek]
 
-                val color = AppUtils.RECYCLER_COLORS[position % RECYCLER_SIZE]
-                Log.d(
-                    "MultipleDaysRecyclerView",
-                    "ViewHolder, bindItems: color=$color, index=${position % RECYCLER_SIZE}"
-                )
-                this.cardView.setBackgroundColor(
-                    ContextCompat.getColor(
-                        binding.root.context,
-                        color
-                    )
-                )
-
-                //todo check gradient
-                val colors = intArrayOf(Color.TRANSPARENT, color.alpha, Color.TRANSPARENT)
-
-                val shape = GradientDrawable(GradientDrawable.Orientation.RIGHT_LEFT, colors)
-                shape.shape = GradientDrawable.OVAL
-                binding.shadowView.background = shape
-                //this.shadowView.setBackgroundColor(ContextCompat.getColor(binding.root.context, R.color.babyGreen))
-
-
-                //todo update with info
+                // Sets card colors
+                settingColors(position)
             }
         }
+
+        /**
+         * Setting color for the background and color with alpha added to it to build the shadow of each card
+         */
+        private fun settingColors(position: Int) {
+            val colorInfo = AppUtils.RECYCLER_COLORS[position % RECYCLER_SIZE]
+            val color = ContextCompat.getColor(binding.root.context, colorInfo)
+            val alphaColor = ColorUtils.setAlphaComponent(color, ALPHA_RECYCLER_SHADOW_COLOR)
+
+            Log.d("MultipleDaysRecyclerView","ViewHolder, bindItems: color=$color, colorInfo=$colorInfo, index=${position % RECYCLER_SIZE}")
+
+            binding.cardView.setBackgroundColor(color)
+            val colors = intArrayOf(Color.TRANSPARENT, alphaColor, Color.TRANSPARENT)
+
+            val shape = GradientDrawable(GradientDrawable.Orientation.RIGHT_LEFT, colors)
+            shape.shape = GradientDrawable.OVAL
+            binding.shadowView.background = shape
+        }
+
     }
 }
--- app/src/main/java/com/filipemorgado/weatherapp_android/utils/Const.kt
+++ app/src/main/java/com/filipemorgado/weatherapp_android/utils/Const.kt
@@ -1,3 +1,4 @@
 package com.filipemorgado.weatherapp_android.utils
 
 const val RECYCLER_SIZE = 5
+const val ALPHA_RECYCLER_SHADOW_COLOR = 128 // 50% Opacity
